2d6e9e34597e01b660d7d992d0f6f4bc75b44f39
Kyle McKenna <mckennakyler@gmail.com>
2020-02-11 03:57:20 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,40 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri' 
+
+#https://learn-co-curriculum.github.io/student-scraper-test-page/index.html
 
 class Scraper
 
   def self.scrape_index_page(index_url)
     
+    scraped_students = [] 
+    
+  html = Nokogiri::HTML(open(index_url)) 
+  #doc = Nokogiri::HTML(open("#https://learn-co-curriculum.github.io/student-scraper-test-page/"))
+  
+  hashes_array = {} #iterate through the student cards id
+  
+  html.css(".student-card").each do |student-card|
+  #doc.css(".post").each do |post|
+    
+    :name <a href="students/{first_name-last_name}.html"> #course = Course.new
+  
+      #course.title = post.css("h2").text
+  
+    :location <p class="student-location">{City, State}</p>
+      #course.schedule = post.css(".date").text
+  
+    #click to view profile(scrape_profile_page) to get profile_urls <h3 class="view-profile-text">View Profile</h3>
+      #course.description = post.css("p").text
+    
+    end 
+    scraped_students << hashes_array #return the array hash 
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    #:profile_url <div class="social-icon-container"> 
+    #                <a href="{Twitter link and/or LinkedIn link and/or GitHub link}">
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,23 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    #send method 
+    @@all << self  
   end
 
   def self.create_from_collection(students_array)
-    
+    #Student.create_from_collection
+    #Scraper.scrape_index_pag
   end
 
   def add_student_attributes(attributes_hash)
+    #student.add_student_attributes
+    #Scraper.scrape_profile_page
     
   end
 
   def self.all
-    
+    @@all 
   end
 end
 
