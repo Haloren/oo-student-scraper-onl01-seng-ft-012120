40e5146ca43c32b4e54daee3903e6a88e1f02720
Kyle McKenna <mckennakyler@gmail.com>
2020-02-23 02:31:23 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,41 +2,22 @@ require 'open-uri'
 require 'pry'
 require 'nokogiri' 
 
-#https://learn-co-curriculum.github.io/student-scraper-test-page/index.html
-
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
-    scraped_students = [] 
-    
-    doc = Nokogiri::HTML(open(index_url)) #doc = Nokogiri::HTML(open(url provided))
-      hash_array = doc.css{'a.card-text-container'}
-    
-    
-      doc.css(".student_card").each do |student_card| #doc.css(".post").each do |post|
-      
-      hash_array ={ #hash_array = iterate through the student cards id 
-      
-      name: student_card.css("a.card-text-container h4.student-name").text, 
-      #<a href=" ..."card-text-container"> <h4 class="student-name">{Student Name}</h4>
-      
-      location: student_card.css("a.card-text-container p.student-location").text, 
-      #<a href=" ..."card-text-container"> <p class="student-location">{City, State}</p>
-      
-      profile_url: student_card.css("a").attribute("href").value 
-      #use ().attribute().value <a href="{link}"> 
-    } 
-    scraped_students << hash_array
-    #binding.pry 
-    end 
-    scraped_students #return the students 
-  end
 
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+    doc.css("div.student-card").each do |card|
+      card.css(".student-card").each do |student|
+        
+      end
+    end
+  end
+  
   def self.scrape_profile_page(profile_url)
     
-    
-  end
+  end   
 
 end
 
